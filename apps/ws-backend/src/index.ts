7075afaba7ec62b4ee2ce6f0d29b9df8a1b77e9e
anshumancodes <anshumanprof01@gmail.com>
2026-08-17 16:04:26 +0530

    feat(ws): implement token authentication

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,26 @@
 import { WebSocketServer, WebSocket } from "ws";
+import { JWT_SECRET } from "./config";
 const wss = new WebSocketServer({ port: 8080 });
+import { JWTPayload, jwtVerify } from "jose";
+export const JWT_KEY = new TextEncoder().encode(JWT_SECRET);
+wss.on("connection", async function connection(ws, request) {
+  const url = request.url;
+  if (!url) {
+    return "no request url found!";
+  }
 
-wss.on("connection", (socket) => {
-  socket.on("message", (message) => {
-    console.log(message.toString());
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") ?? " ";
+  const decoded = await jwtVerify(token, JWT_KEY);
+  if (typeof decoded == "string") {
+    ws.close();
+    return;
+  }
+  if (!decoded) {
+    ws.close();
+    return;
+  }
+  ws.on("message", (message) => {
+    ws.send("pong");
   });
 });
