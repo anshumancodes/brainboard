f6586b8c4113cd98b3b46457e8358a197af34d7a
anshumancodes <anshumanprof01@gmail.com>
2026-08-22 03:45:23 +0530

    feat(room): add roomId param to get chats

--- apps/http-backend/src/routes/room.routes.ts
+++ apps/http-backend/src/routes/room.routes.ts
@@ -5,6 +5,6 @@ import { createRoom, getChats } from "../controllers/room.controller.js";
 const router: Router = Router();
 
 router.post("/create", authMiddleware, createRoom);
-router.get("/chats", authMiddleware, getChats);
+router.get("/chats/:roomId", authMiddleware,getChats);
 
 export default router;
