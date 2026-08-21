e8565ac34031a33f769a1f8807d63da404ec8735
anshumancodes <anshumanprof01@gmail.com>
2026-08-22 01:35:01 +0530

    fix(database): add missing semicolons

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,11 +1,11 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client.js";
 
 dotenv.config({
   path: new URL("../.env", import.meta.url),
 });
-console.log(process.env.DATABASE_URL)
+console.log(process.env.DATABASE_URL);
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
