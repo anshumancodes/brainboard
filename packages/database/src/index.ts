5a537d6eff316b932d732371bc5049a97517bcb5
anshumancodes <anshumanprof01@gmail.com>
2026-08-21 16:32:10 +0530

    feat(database): load .env for DB connection

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,10 +1,13 @@
+import dotenv from "dotenv"
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client.js";
 
-import type { User } from "./generated/prisma/client.js";
-
+dotenv.config({
+  path: new URL("../.env", import.meta.url),
+});
+console.log(process.env.DATABASE_URL)
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export const prisma = new PrismaClient({
