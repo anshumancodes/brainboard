a9e7985cc2c0ec83b59613b18bc50566d74b8bea
anshumancodes <anshumanprof01@gmail.com>
2026-08-22 04:06:19 +0530

    feat(room): add getRoomIdfromSlug route

--- apps/http-backend/src/routes/room.routes.ts
+++ apps/http-backend/src/routes/room.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/middleware.js";
-import { createRoom, getChats } from "../controllers/room.controller.js";
+import {
+  createRoom,
+  getChats,
+  getRoomIdfromSlug,
+} from "../controllers/room.controller.js";
 
 const router: Router = Router();
 
 router.post("/create", authMiddleware, createRoom);
-router.get("/chats/:roomId", authMiddleware,getChats);
+router.get("/chats/:roomId", authMiddleware, getChats);
+router.get("/slug/:slug", authMiddleware, getRoomIdfromSlug);
 
 export default router;
