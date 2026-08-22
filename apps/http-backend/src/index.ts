716df031c7e37f60e15282edc22a74d6fa4dd35c
anshumancodes <anshumanprof01@gmail.com>
2026-08-23 01:18:44 +0530

    feat(http-backend): add cors middleware

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import roomRouter from "./routes/room.routes.js";
 import userRouter from "./routes/user.routes.js";
+import cors from "cors";
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/room", roomRouter);
