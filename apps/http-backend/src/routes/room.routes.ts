fe5bd011f2ff263672047f1e01e16c4f26580036
anshumancodes <anshumanprof01@gmail.com>
2026-09-02 10:49:27 +0530

    feat(room): add delete shape route

--- apps/http-backend/src/routes/room.routes.ts
+++ apps/http-backend/src/routes/room.routes.ts
@@ -6,6 +6,7 @@ import {
   getMyRooms,
   deleteRoom,
   getShapes,
+  deleteShape,
 } from "../controllers/room.controller.js";
 
 const router: Router = Router();
@@ -13,6 +14,7 @@ const router: Router = Router();
 router.post("/create", authMiddleware, createRoom);
 router.get("/my-rooms", authMiddleware, getMyRooms);
 router.get("/shapes/:roomId", authMiddleware, getShapes);
+router.delete("/shapes/:shapeId", authMiddleware, deleteShape);
 router.get("/slug/:slug", authMiddleware, getRoomIdfromSlug);
 router.delete("/:id", authMiddleware, deleteRoom);
 
