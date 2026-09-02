ac77319a9cbdbfbaa17e26e68cf3a98196848d7b
anshumancodes <anshumanprof01@gmail.com>
2026-09-02 09:43:02 +0530

    feat(room): add getShapes route

--- apps/http-backend/src/routes/room.routes.ts
+++ apps/http-backend/src/routes/room.routes.ts
@@ -2,17 +2,17 @@ import { Router } from "express";
 import authMiddleware from "../middlewares/middleware.js";
 import {
   createRoom,
-  getChats,
   getRoomIdfromSlug,
   getMyRooms,
   deleteRoom,
+  getShapes,
 } from "../controllers/room.controller.js";
 
 const router: Router = Router();
 
 router.post("/create", authMiddleware, createRoom);
 router.get("/my-rooms", authMiddleware, getMyRooms);
-router.get("/chats/:roomId", authMiddleware, getChats);
+router.get("/shapes/:roomId", authMiddleware, getShapes);
 router.get("/slug/:slug", authMiddleware, getRoomIdfromSlug);
 router.delete("/:id", authMiddleware, deleteRoom);
 
