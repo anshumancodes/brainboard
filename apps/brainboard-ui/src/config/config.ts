49b8387df1af1da84b0dd318b59dbf0d9fe2e8d5
anshumancodes <anshumanprof01@gmail.com>
2026-09-11 11:56:38 +0530

    feat(config): Use environment variables for URLs

--- apps/brainboard-ui/src/config/config.ts
+++ apps/brainboard-ui/src/config/config.ts
@@ -1,3 +1,11 @@
-const WS_URL = "ws://localhost:8080";
-const HTTP_BACKEND_URL = "http://localhost:8000";
+const isDevelopment = process.env.ENVIROMENT === "DEV";
+
+const WS_URL = isDevelopment
+  ? "ws://localhost:8080"
+  : "wss://ws.brainboard.anshumancdx.xyz";
+
+const HTTP_BACKEND_URL = isDevelopment
+  ? "http://localhost:8000"
+  : "https://brainboard.anshumancdx.xyz";
+
 export { WS_URL, HTTP_BACKEND_URL };
