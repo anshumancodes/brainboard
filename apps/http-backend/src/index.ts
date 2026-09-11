d1ca9e304dab68685f067ba2b4551b006f2f53f9
anshumancodes <anshumanprof01@gmail.com>
2026-09-11 11:22:48 +0530

    feat(http-backend): configure cors origin dynamically

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,11 +2,12 @@ import express from "express";
 import roomRouter from "./routes/room.routes.js";
 import userRouter from "./routes/user.routes.js";
 import cors from "cors";
+import { ENVIROMENT, DEV_CORS_ORIGIN, PROD_CORS_ORIGIN } from "@repo/backend-common/config";
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ENVIROMENT == "DEV" ? DEV_CORS_ORIGIN : PROD_CORS_ORIGIN,
     credentials: true,
   }),
 );
